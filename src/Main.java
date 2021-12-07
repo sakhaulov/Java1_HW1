19f9bcceeb8b41417da3a49dd80abdb90d31882d
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2021-12-07 07:43:57 +0500

    Optimized printColor.

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,11 @@ public class Main {
     }
 
     public static void printColor(int value) {
-        if (value < 0) {
+        if (value <= 0) {
             System.out.println("Красный");
-        } else if (value > 0 && value < 100) {
+        } else if (value > 0 && value <= 100) {
             System.out.println("Жёлтый");
-        } else if (value > 100) {
+        } else {
             System.out.println("Зелёный");
         }
 
